586cf53848afc294fc120e54380649c7372b2813
orehren <o.rehren@gmail.com>
2025-11-06 06:09:46 +0100

    clean directory and move assets into the extension folder

--- _extensions/academiccvtemplate/lua/correct-path.lua
+++ _extensions/academiccvtemplate/lua/correct-path.lua
@@ -4,11 +4,20 @@ correct-path.lua – prepends a relative path to the profile-photo metadata fiel
 
 function Pandoc (doc)
   local meta = doc.meta
-  if meta['profile-photo'] then
-    local path = pandoc.utils.stringify(meta['profile-photo'])
-    if path ~= "" then
-      meta['profile-photo'] = pandoc.MetaInlines(pandoc.Str("../../../" .. path))
-    end
+
+  -- Guard Clause 1: does meta entry exist?
+  if not meta['profile-photo'] then
+    return doc
   end
+
+  local path = pandoc.utils.stringify(meta['profile-photo'])
+
+  -- Guard Clause 2: is entry empty?
+  if path == "" then
+    return doc
+  end
+
+  meta['profile-photo'] = pandoc.MetaInlines(pandoc.Str("../../../" .. path))
+
   return doc
 end
